648958476addd9b3328487a27edea00076c5900b
hoanh <hoanhphap582002@gmail.com>
2024-11-08 17:10:09 +0700

    Flappy-Bird

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int boarWidth = 360;
+        int boarHeight = 640;
+
+        JFrame frame = new JFrame("Flappy-Bird");
+        frame.setSize(boarWidth, boarHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        FlappyBird flappyBird = new FlappyBird();
+        frame.add(flappyBird);
+        frame.pack();
+        flappyBird.requestFocus();
+        frame.setVisible(true);
+    }
+}
